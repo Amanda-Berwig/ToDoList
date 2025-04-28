cc667ebe934f3431b2b2ff2f9251f09822c223e7
Amanda Berwig <amanda.berwig@gmail.com>
2025-04-28 18:12:33 -0300

    Ajusta titulo do card

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -4,21 +4,18 @@ import TaskInputField from "./TaskForm/TaskInputField";
 import Card from "./UI/Card";
 import { useTasks } from "../hooks/useTasks";
 
-
 function MainSection() {
   const {
     taskList,
     handleAddTask,
     handleCheckTask,
     handleUpdateTaskDescription,
-    handleRemoveTask
+    handleRemoveTask,
   } = useTasks();
 
   return (
     <Card>
       <TaskInputField onAddTask={handleAddTask} />
-      
-      <h1 className="mt-4 text-center font-bold">Lista de Tarefas</h1>
       <TaskList
         data={taskList}
         onCheckTask={handleCheckTask}
--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -4,11 +4,14 @@ const Card = ({ children }) => {
   const { theme } = useTheme();
   return (
     <div
-      className={`max-w-md w-full rounded-xl shadow-lg p-6 ${theme === "light" ? "bg-[#FFFFFF]" : "bg-[#d7dbe0]"}`}
+      className={`max-w-md w-full rounded-xl shadow-lg p-6 ${
+        theme === "light" ? "bg-[#FFFFFF]" : "bg-[#d7dbe0]"
+      }`}
     >
+      <h1 className="mt-4 text-center font-bold">Lista de Tarefas</h1>
       {children}
     </div>
-  )
+  );
 };
 
 export default Card;
