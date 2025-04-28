a48df2065c3326d2c39b748f7b15bff6c3766e46
Amanda Berwig <amanda.berwig@gmail.com>
2025-04-28 16:21:57 -0300

    WIP

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -4,43 +4,13 @@ import TaskForm from "./TaskForm";
 import AddButton from "./AddButton";
 import { useTheme } from "./ThemeContext";
 import DeleteButton from "./DeleteButton";
+import TaskList from "./TaskList";
+
+const demoTasks = [{ id: "string1", task: "implementar lista", done: false }];
 
 function MainSection() {
-  const [task, setTask] = useState("");
-  const [listTasks, setListTasks] = useState([]);
-  const [modoEdição, setModoEdicao] = useState(false);
-  const [textoEditando, setTextoEditando] = useState(""); // guarda o novo valor digitado
+  const [taskList, setTaskList] = useState(demoTasks);
   const { theme } = useTheme();
-
-  const handleStartEdit = (item) => {
-    setModoEdicao(item.id);
-    setTextoEditando(item.task); // pré-preenche o input com o valor atual
-  };
-
-  const handleEdit = (event, item) => {
-    if (event.key === "Enter") {
-      const novaLista = listTasks.map((tarefa) =>
-        tarefa.id === item.id ? { ...tarefa, task: textoEditando } : tarefa
-      );
-      setListTasks(novaLista);
-      setModoEdicao(null);
-    } else if (event.key === "Escape") {
-      setModoEdicao(null); // cancela edição
-    }
-  };
-
-  const deleteTask = (id) => {
-    const novaLista = listTasks.filter((tarefa) => tarefa.id !== id);
-    setListTasks(novaLista);
-  };
-
-  const toggleDone = (id) => {
-    const novaLista = listTasks.map((tarefa) =>
-      tarefa.id === id ? { ...tarefa, done: !tarefa.done } : tarefa
-    );
-    setListTasks(novaLista);
-  };
-
   return (
     <div
       className={`max-w-md w-full rounded-xl shadow-lg p-6 ${
@@ -48,67 +18,10 @@ function MainSection() {
       }`}
     >
       <h1 className="text-center font-bold">Lista de Tarefas</h1>
-      <div className="flex items-center gap-5 m-5">
-        <TaskForm task={task} setTask={setTask} />
-        <AddButton
-          task={task}
-          setTask={setTask}
-          listTasks={listTasks}
-          setListTasks={setListTasks}
-        />
-      </div>
+      <TaskList taskList={taskList} />
+      {/* <div className="flex items-center gap-5 m-5">
 
-      <ul className="space-y-2 ">
-        {listTasks.map((item) => (
-          <li
-            key={item.id}
-            onDoubleClick={() => handleStartEdit(item)}
-            className={`relative p-4 rounded-lg ${
-              theme === "light"
-                ? " bg-gray-50  border-gray-100"
-                : "bg-[#cad0d5] border-[#d7dbe0]"
-            } shadow-sm border hover:shadow-md transition-shadow`}
-          >
-            {modoEdição === item.id ? (
-              <div className="flex items-center justify-between">
-                <input
-                  className="border px-2 py-1 rounded w-full "
-                  value={textoEditando}
-                  onChange={(e) => setTextoEditando(e.target.value)}
-                  onKeyDown={(e) => handleEdit(e, item)}
-                  autoFocus
-                />
-                <div className="flex gap-2"></div>
-              </div>
-            ) : (
-              <div
-                className="flex items-center gap-2"
-                onClick={() => toggleDone(item.id)}
-              >
-                {item.done ? (
-                  <CircleCheckBig strokeWidth="1.5px" />
-                ) : (
-                  <Circle strokeWidth="1.5px" />
-                )}
-                <span
-                  className={
-                    item.done ? "line-through text-gray-500" : "text-gray-800"
-                  }
-                >
-                  {item.task}
-                </span>
-              </div>
-            )}
-            <DeleteButton onDelete={() => deleteTask(item.id)} />
-          </li>
-        ))}
-      </ul>
-
-      {listTasks.length === 0 && (
-        <p className="text-center text-gray-500 py-4">
-          Nenhuma tarefa disponível. Adicione sua primeira tarefa!
-        </p>
-      )}
+      </div> */}
     </div>
   );
 }
